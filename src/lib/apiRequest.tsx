a6478d4dd7d14fe4f0505428eb5405e95831f667
AFIF ZILANI <afifzilani4566@gmail.com>
2025-01-16 20:33:31 +0600

    add: (useUpdateEntireData, useUpdateData, useDeleteData) hook and remove  parametere from  hook

--- src/lib/apiRequest.tsx
+++ src/lib/apiRequest.tsx
@@ -6,7 +6,9 @@ const axiosInstance = axios.create({
     headers: { "Content-Type": "application/json" },
 });
 
-export function useGetData(endpoint: string, name: string) {
+// Hook for fetching data
+export function useGetData(endpoint: string) {
+    const name = endpoint.split("/")[1];
     const fetchData = async () => {
         const response = await axiosInstance.get(endpoint);
         return response.data;
@@ -15,16 +17,64 @@ export function useGetData(endpoint: string, name: string) {
     return useQuery(name, fetchData);
 }
 
-export function useAddData(endpoint: string, name: string) {
-  console.log("react query:", endpoint)
+// Hook for adding data
+export function useAddData(endpoint: string) {
     const queryClient = useQueryClient();
-
+    const name = endpoint.split("/")[1];
     const addData = async (data: any) => {
         const response = await axiosInstance.post(endpoint, data);
         return response.data;
     };
 
     return useMutation(addData, {
+        onSuccess: () => {
+            queryClient.invalidateQueries(name);
+        },
+    });
+}
+
+// Hook for deleting data
+export function useDeleteData(endpoint: string) {
+    const queryClient = useQueryClient();
+    const name = endpoint.split("/")[1];
+    const deleteData = async (id: string) => {
+        const response = await axiosInstance.delete(endpoint);
+        return response.data;
+    };
+
+    return useMutation(deleteData, {
+        onSuccess: () => {
+            queryClient.invalidateQueries(name);
+        },
+    });
+}
+
+// Hook for updating data using PATCH
+export function useUpdateData(endpoint: string) {
+    const queryClient = useQueryClient();
+    const name = endpoint.split("/")[1];
+    const updateData = async (data: any) => {
+        const response = await axiosInstance.patch(endpoint, data);
+        return response.data;
+    };
+
+    return useMutation(updateData, {
+        onSuccess: () => {
+            queryClient.invalidateQueries(name);
+        },
+    });
+}
+
+// Hook for updating entire data using PUT
+export function useUpdateEntireData(endpoint: string) {
+    const queryClient = useQueryClient();
+    const name = endpoint.split("/")[1];
+    const updateEntireData = async (data: any) => {
+        const response = await axiosInstance.put(endpoint, data);
+        return response.data;
+    };
+
+    return useMutation(updateEntireData, {
         onSuccess: () => {
             queryClient.invalidateQueries(name);
         },
